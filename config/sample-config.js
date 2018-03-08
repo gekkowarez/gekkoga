accd2c9763434ed36c560d20c09d9efb5618f7e0
generalectric <jmbeck210@gmail.com>
2018-03-07 22:57:10 -0600

    fix sample-config to reflect latest changes

--- config/sample-config.js
+++ config/sample-config.js
@@ -1,7 +1,7 @@
 const randomExt = require('random-ext');
 
 const config = {
-  stratName: 'custom_macd',
+  stratName: 'MACD',
   gekkoConfig: {
     watch: {
       exchange: 'poloniex',
@@ -9,13 +9,13 @@ const config = {
       asset: 'BTC'
     },
 
-    daterange: 'scan',
-/*
+//    daterange: 'scan',
+
     daterange: {
-      from: '2017-01-01 00:00',
-      to: '2017-12-05 00:00'
+      from: '2018-01-01 00:00',
+      to: '2018-02-01 00:00'
     },
-*/
+
     simulationBalance: {
       'asset': 1,
       'currency': 1
@@ -41,32 +41,36 @@ const config = {
   // How many parallel queries to run at once
   parallelqueries: 5,
 
-  // profit || score 
-  // score = profit * sharpe -- feedback?
+  // profit || score
+  // score = ideas? feedback?
   // profit = recommended!
   mainObjective: 'profit',
 
   // optionally recieve and archive new all time high every new all time high
   notifications: {
     email: {
-      enabled: true,
-      receiver: 'receiver@some.com',
+      enabled: false,
+      receiver: 'destination@some.com',
       senderservice: 'gmail',
-      sender: 'sender@gmail.com',
+      sender: 'origin@gmail.com',
       senderpass: 'password',
     },
   },
+
   candleValues: [5,10,15,30,60,120,240],
   getProperties: () => ({
-    // Strat settings must be flattened and cannot be nested for mutation to work properly!
 
     historySize: randomExt.integer(100, 20),
 
-    macd_short: randomExt.integer(15,5),
-    macd_long: randomExt.integer(40,15),
-    macd_signal: randomExt.integer(12,6),
-    macd_up: randomExt.float(20,0).toFixed(2),
-    macd_down: randomExt.float(0,-20).toFixed(2),
+    short: randomExt.integer(15,5),
+    long: randomExt.integer(40,15),
+    signal: randomExt.integer(12,6),
+
+    thresholds: {
+      up: randomExt.float(20,0).toFixed(2),
+      down: randomExt.float(0,-20).toFixed(2),
+      persistence: randomExt.integer(9,0),
+    },
 
     candleSize: randomExt.pick(config.candleValues)
   })
